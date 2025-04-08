d511f9d75d06331fa1fd4179a8d1125e7dd91f19
Sebastián Ortiz <ortiz_seba@icloud.com>
2025-04-07 21:29:14 -0300

    Fixed main

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -6,30 +5,17 @@ from src.config.database import Database
 from src.controllers.health_check import router as health_check_controller
 from src.controllers.user import UserController
 
-
-def create_fastapi_app():
-    fastapi_app = FastAPI(
-        title="FastAPI Application",
-    )
-
-    fastapi_app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-    fastapi_app.include_router(health_check_controller, prefix="/health_check")
-    fastapi_app.include_router(UserController().router, prefix="/user")
-
-    return fastapi_app
-
-
-def run_app(fastapi_app: FastAPI):
-    uvicorn.run(fastapi_app, host="localhost", port=8000)
-
-
 db = Database()
-app = create_fastapi_app()
-run_app(app)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(health_check_controller, prefix="/health_check")
+app.include_router(UserController().router, prefix="/user")
